ce6dcdd15a2d8ff8ce321e2fd88c18bad691708a
leucopus <hollandlop22@outlook.com>
2025-12-28 14:27:38 -0500

    Git Saving-- staging,reverting and checkout

---
+++ restorepoint..js
@@ -0,0 +1,12 @@
+//this code is working and i want to save it
+//  before making further changes.
+
+
+1.// ___BEFORE MAKING CHANGES: "git add ." | "git add <filename>"
+2.//___COMMIT FILE
+// ...EDIT FILE
+
+1.5//TO UPDATE EDIT TO STAGED FILE: "git add ."
+1.6//TO REVERT EDITED-STAGED FILE: git revert --staged <filename>
+
+2.//____TO FORMAT BACK/LOAD STAGED FILE: "git checkout -- <filename>"
